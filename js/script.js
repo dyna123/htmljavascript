76928acba0c485396214558afaa4d1b6105e7adb
idham <idhamkholid965@gmail.com>
2019-09-20 18:38:55 +0700

    add arrow function

--- js/script.js
+++ js/script.js
@@ -66,33 +66,27 @@
 
 
 // let Idham = new Mahasiswa('Idham Kholid', 10);
+// class Mahasiswa {
+//     constructor(nama, energi) {
+//         this.nama = nama;
+//         this.energi = energi;
+//     }
+//     makan(porsi) {
+//         this.energi += porsi;
+//         return `Halo ${this.nama}, selamat makan!`;
+//     }
+//     main(jam) {
+//         this.energi -= jam;
+//         return `Halo ${this.nama}, selamat bermain!`;
+//     }
 
-class Mahasiswa {
-    constructor(nama, energi) {
-        this.nama = nama;
-        this.energi = energi;
-    }
-    makan(porsi) {
-        this.energi += porsi;
-        return `Halo ${this.nama}, selamat makan!`;
-    }
-    main(jam) {
-        this.energi -= jam;
-        return `Halo ${this.nama}, selamat bermain!`;
-    }
+//     tidur(jam) {
+//         this.energi += jam * 2;
+//         return `Halo ${this.nama}, selamat tidur!`;
+//     }
+// }
 
-    tidur(jam) {
-        this.energi += jam * 2;
-        return `Halo ${this.nama}, selamat tidur!`;
-    }
-}
-
-let Idham = new Mahasiswa('Idham Kholid', 10);
-
-
-
-
-
+// let Idham = new Mahasiswa('Idham Kholid', 10);
 
 // // 3. Constructor function
 // // keyword new
@@ -109,7 +103,54 @@ let Idham = new Mahasiswa('Idham Kholid', 10);
 //         console.log(`Halo ${this.nama}, selamat bermain!`);
 //     }
 // }
-
-
 // let idham = new Mahasiswa('Idham Kholid', 10);
 
+// 4 Execution Context, hoisting dan scope
+// function init() {
+//     let nama = 'Idham Kholid';
+//     function tampilNama() {
+//         console.log(nama);
+//     }
+//     tampilNama();
+// }
+// init();
+
+// let add = (function () {
+//     let counter = 0;
+//     return function () {
+//         return ++counter;
+//     }
+// })();
+
+
+// console.log(add());
+// console.log(add());
+// console.log(add());
+// console.log(add());
+// console.log(add());
+
+// const tampilNama = (nama, waktu) => {
+//     return `Selamat ${waktu}, ${nama}`;
+// }
+
+// console.log(tampilNama('Idham', 'Pagi'));
+
+// implisit return
+// const tampilNama = nama => `Halo ${nama}`;
+// console.log('idham');
+
+// const tampilNama = () => `Hello World`;
+// console.log(tampilNama());    
+
+let mahasiswa = ['Idham Kholid', 'Fajar', 'Yusuf'];
+
+// let jumlahHuruf = mahasiswa.map(function (nama) {
+//     return nama.length;
+// });
+// console.log(jumlahHuruf);
+
+// let jumlahHuruf = mahasiswa.map(nama => nama.length);
+// console.log(jumlahHuruf);
+
+let jumlahHuruf = mahasiswa.map(nama => ({ nama, jmlHuruf: nama.length }));
+console.table(jumlahHuruf)
